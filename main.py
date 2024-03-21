959acdf084621b449bf473fe0caf883bb58b178a
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2024-03-21 10:12:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from discord.ext import commands
 import sqlite3
 from key_generator.key_generator import generate
 import discord
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+
 
 
 connection = sqlite3.connect("database.db")
